700a9af7d55aa05e2bb8f6d1b7c3c7ad853a48ed
Papa Badu <raybadu10@gmail.com>
2024-11-29 00:32:52 +0000

    update

--- VPN_International.cpp
+++ VPN_International.cpp
@@ -1,2 +1,34 @@
 #include "VPN_International.h"
+using namespace System;
+using namespace System::Windows::Forms;
 
+int main(array<String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    FinalProjectVPN::VPN_International form;  // Potential issue here
+    Application::Run(% form);
+}
+
+using namespace MySql::Data::MySqlClient;
+public ref class MyForm : public System::Windows::Forms::Form {
+    MySqlConnection^ sqlConn = gcnew MySqlConnection();
+    MySqlCommand^ sqlCmd = gcnew MySqlCommand();
+    DataTable^ sqlDt = gcnew DataTable();
+    MySqlDataAdapter^ sqlDtA = gcnew MySqlDataAdapter();
+
+public:
+    MySqlDataReader^ sqlRd;
+
+    sqlConn->ConnectionString = "datasource=localhost;port=3306;username=root;password=;database=ashesi";
+    sqlConn->Open();
+    sqlCmd->Connection = sqlConn;
+    sqlCmd->CommandText = "SELECT * FROM employee";
+    sqlRd = sqlCmd->ExecuteReader();
+    sqlDt->Load(sqlRd);
+    sqlRd->Close();
+    sqlConn->Close();
+    dataGridView1->DataSource = sqlDt;
+
+
+
+};
